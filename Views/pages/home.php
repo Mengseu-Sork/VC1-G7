6fa3d55539c5d3a88915f6f7c5a50e5ad1b2df69
sokleap <sokleap808@gmail.com>
2025-03-10 20:40:02 +0700

    pull code

--- Views/pages/home.php
+++ Views/pages/home.php
@@ -2,4 +2,3 @@
 require_once '../layout/navbarPages/header_user.php';
 require_once '../layout/navbarPages/nav_user.php';
 require_once '../layout/navbarPages/footer_user.php';
-?>
--- index.php
+++ index.php
@@ -1,2 +1,4 @@
 <?php
 require "./Router/route.php";
+
+// require "./Views/pages/home.php";
