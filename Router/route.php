8afcf138747bd20bae47d5c6c1add465ea9b04fa
unknown <Mealea.Meng@pnc.lan>
2025-03-11 13:25:29 +0700

    buge(product list: page)

--- Router/route.php
+++ Router/route.php
@@ -1,25 +1,13 @@
 <?php
 require 'Router.php';
-require_once 'Controllers/UserController.php';
+require 'Controllers/UserController.php';
 require_once 'Controllers/DashboardController.php';
-<<<<<<< HEAD
 require 'Controllers/ProductController.php';
-=======
-require_once 'Controllers/AdminController.php';
->>>>>>> 114b77203b1b04157aaeb5dc5d8f54ce0bc66422
 
 
 $routes = new Router();
 
 $routes->get('/', [DashboardController::class, 'index']);
-
-
-$routes->get('/admin', [AdminController::class, 'index']);
-$routes->get('/admin/create', [AdminController::class, 'create']);
-$routes->post('/admin/store', [AdminController::class, 'store']);
-$routes->get('/admin/edit', [AdminController::class, 'edit']);
-$routes->put('/admin/update', [AdminController::class, 'update']);
-$routes->delete('/admin/delete', [AdminController::class, 'destroy']);
 
 $routes->get('/user', [UserController::class, 'index']);
 $routes->get('/user/create', [UserController::class, 'create']);
@@ -31,8 +19,7 @@ $routes->get('/user/show', [UserController::class, 'show']);
 
 $routes->get('/products', [ProductController::class, 'index']);
 $routes->get('/products/product_list', [ProductController::class, 'index']);
-$routes->get('/products/create', [ProductController::class, 'create']);
-$routes->get('/products/edite', [ProductController::class, 'edite']);
+$routes->get('/products/create', [UserController::class, 'create']);
 $routes->delete('/products/delete', [ProductController::class, 'delete']);
 
 $routes->dispatch();
