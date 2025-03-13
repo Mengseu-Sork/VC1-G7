b2a57edbc5d806d99e30272caabe20df31e2745f
Kartrork <kartrork1551@gmail.com>
2025-03-13 07:55:26 +0700

    feat(use) :code error

--- Databases/Database.php
+++ Databases/Database.php
@@ -6,37 +6,41 @@ if (!class_exists('Database')) {
         private static $instance = null;
         private $pdo;
 
-        try {
+        private function __construct($servername, $dbname, $username, $password)
+        {
+            try {
+                $this->pdo = new PDO("mysql:host=$servername;dbname=$dbname;charset=utf8", $username, $password, [
+                    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+                ]);
+            } catch (PDOException $e) {
+                die("Connection failed: " . $e->getMessage());
+            }
+        }
 
-            $this->pdo = new PDO("mysql:host=$servername;dbname=$dbname;charset=utf8", $username, $password);
-            $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        public static function getInstance($servername, $dbname, $username, $password)
+        {
+            if (self::$instance === null) {
+                self::$instance = new self($servername, $dbname, $username, $password);
+            }
+            return self::$instance;
+        }
 
-            $this->pdo = new PDO("mysql:host=$servername;dbname=$dbname;charset=utf8", $username, $password, [
-                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-            ]);
+        public function query($sql, $params = [])
+        {
+            try {
+                $stmt = $this->pdo->prepare($sql);
+                $stmt->execute($params);
+                return $stmt;
+            } catch (PDOException $e) {
+                die("Query Error: " . $e->getMessage());
+            }
+        }
 
-        } catch (PDOException $e) {
-            die("Connection failed: " . $e->getMessage());
+        public function closeConnection()
+        {
+            $this->pdo = null;
         }
     }
-    public function query($sql, $params = [])
-    {
-        try {
-            $stmt = $this->pdo->prepare($sql);
-            $stmt->execute($params);
-            return $stmt;
-        } catch (PDOException $e) {
-
-            die("Query Error: " . $e->getMessage());
-        }
-    }
-
-
-    public function closeConnection()
-    {
-        $this->pdo = null;
-    }
-
 }
 ?>
