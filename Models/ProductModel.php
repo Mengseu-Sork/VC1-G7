2af25bef4cb0c1d42792c869c6c5f5f871f09d46
thavry-chanhak <thavry.chanhak@student.passerellesnumeriques.org>
2025-03-16 14:34:39 +0700

    refactor(update products) : Clean update product function [CS-46]

--- Models/ProductModel.php
+++ Models/ProductModel.php
@@ -1,17 +1,28 @@
 <?php
-require_once "Database.php"; // Adjust the path as necessary
+require_once 'Databases/database.php';
 
+// ProductModel.php
 class ProductModel {
     private $db;
 
     public function __construct() {
-        $this->db = new Database();
+        $this->db = new Database(); 
     }
 
-    public function getAllProducts() {
-        return $this->db->query("SELECT * FROM product")->fetchAll();
+    // Get all products
+    function getAllProducts() {
+        $query = "SELECT * FROM product";
+        $result = $this->db->query($query);
+        return $result->fetchAll();
     }
 
-   
+    // Add a product (example function)
+    function addProduct($name, $price) {
+        $query = "INSERT INTO product (name, price) VALUES (?, ?)";
+        $stmt = $this->db->prepare($query);
+        return $stmt->execute([$name, $price]);
+    }
+
+    
 }
 ?>
