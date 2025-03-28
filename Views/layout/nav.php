254d626555f6556d478b4a08690cd03a8d49d184
Kartrork <kartrork1551@gmail.com>
2025-03-28 09:33:09 +0700

    feat(product)

--- Views/layout/nav.php
+++ Views/layout/nav.php
@@ -62,7 +62,7 @@
               <div>
                 <!-- active classes 'bg-primary-100 dark:bg-primary' -->
                 <a
-                  href="pages/stock"
+                  href="/pages/stock"
                   class=".sidebar-item flex items-center p-2 text-gray-500 transition-colors rounded-md dark:text-light hover:bg-primary-100 dark:hover:bg-primary"
 
                 >
